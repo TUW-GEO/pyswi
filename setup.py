9f6dba5e59f1aec81f04153ce8346cc136b3c02c
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2017-03-09 17:13:55 +0100

    created egg info

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+          tests_require=['test_swi'],
           use_pyscaffold=True)
 
 
