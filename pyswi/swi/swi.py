64f75b0f588f85989035809f12220e591d8fd595
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2017-03-14 17:49:23 +0100

    Added np array parameters and added date_begin and date_end functionality

--- pyswi/swi/swi.py
+++ pyswi/swi/swi.py
@@ -30,7 +30,7 @@ float64_nan = -999999.
 float32_nan = -999999.
 
 
-def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
+def process_uf_pd(ssm_userformat_data, proc_param={}, ctime=[1, 5, 10, 15, 20, 40, 60, 100], gain_in=None):
     """
     Processing of surface soil water index and the gain value.
     This function calls the calculate function and handles the conversions of the input/output formats of the gain to
@@ -41,9 +41,11 @@ def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
     ssm_userformat_data : pandas.DataFrame
         Surface soil moisture userdata time series including ssm and ssf values.
     proc_param : dict
-        Processing parameters.
+        Processing parameters. e.g. date_begin, date_end (julian date)
     swi_param : dict
         Soil Water Index parameters.
+    ctime : list
+        Ctime values that should be calculated.
     gain_in : pandas.DataFrame
          Input gain parameters of last calculation.
          fields gpi, last_jd, nom, denom
@@ -58,12 +60,14 @@ def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
         fields gpi, last_jd(= date index), NOM_xxx, DENOM_xxx (e.g. NOM_010 for ctime value 10)
     """
 
-    ssm_ts = {'jd': ssm_userformat_data.axes[0].values,
+    juldate = pd_datetime_to_juldate(ssm_userformat_data.axes[0].values)
+
+    ssm_ts = {'jd': juldate,
               'sm': ssm_userformat_data['sm'].values}
 
-    ssf_ts = {'ssf': ssm_userformat_data['ssf'].values}
+    ssf_ts = ssm_userformat_data['ssf'].values
 
-    if 'data_from' in proc_param and 'date_to' in proc_param:
+    if 'date_from' in proc_param and 'date_to' in proc_param:
         date_from = proc_param['date_from']
         date_to = proc_param['date_to']
     else:
@@ -74,23 +78,15 @@ def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
     num_swi = num_swi.astype('timedelta64[D]')
     num_swi = num_swi.astype(int)+1
 
-    if swi_param['ctime'] is None:
-        ctime = np.array([1, 5, 10, 15, 20, 40, 60, 100])
-    else:   # Parsing list items from String
-        ctime = swi_param['ctime']
-        ctime = ctime[1:-1]
-        ctime = np.fromstring(ctime, dtype=int, sep=',')
+    if type(ctime) is not np.array:
+        ctime = np.array(ctime)
 
     # Init empty swi_ts
     swi_ts = {'jd': np.zeros([num_swi], dtype=np.float64),
               'swi': np.zeros([num_swi, len(ctime)], dtype=np.float32),
               'qflag': np.zeros([num_swi, len(ctime)], dtype=np.float32)}
 
-    date_buffer = pd.DatetimeIndex([date_from])
-
-    start_date = julian.julday(date_buffer.month[0], date_buffer.day[0], date_buffer.year[0], 00, 01)
-
-    swi_dates = np.arange(num_swi) + start_date
+    swi_dates = np.arange(num_swi) + date_from
 
     swi_ts['jd'] = swi_dates
 
@@ -99,7 +95,13 @@ def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
     if gain_in is not None:
         gain = gain_dataframe_to_dict(gain_in, ctime)
 
-    swi_ts, gain_out_mp = calc(ssm_ts, ssf_ts, swi_ts, ctime=ctime, gain=gain)
+    if num_swi != len(juldate):
+        indices = [i for i, x in enumerate(ssm_ts['jd']) if x == date_from]
+        startdate_idx = indices[0]
+        ssm_ts['sm'] = ssm_userformat_data['sm'].values[startdate_idx:]
+        ssm_ts['jd'] = juldate[startdate_idx:]
+
+    swi_ts, gain_out_mp = calc(ssm_ts, swi_ts, ssf_ts=ssf_ts, ctime=ctime, gain=gain)
 
     swi_ts = swi_dict_to_dataframe(swi_ts, ctime)
 
@@ -108,7 +110,85 @@ def process(ssm_userformat_data, proc_param, swi_param, gain_in=None):
     return swi_ts, gain
 
 
-def calc(ssm_ts, ssf_ts, swi_ts, gain=None, ctime=None):
+def process_swi(ssm, jd, ssf_ts=None, proc_param={}, ctime=[1, 5, 10, 15, 20, 40, 60, 100], gain_in=None):
+    """
+    Processing of surface soil water index and the gain value.
+    This function calls the calculate function and handles the conversions of the input/output formats of the gain to
+    the formats that are needed for the calculation.
+
+    Parameters
+    ----------
+    ssm : numpy.ndarray
+        Surface soil moisture time series.
+    jd : numpy.ndarray
+        Julian dates time series.
+    ssf : numpy.ndarray
+        Surface State Flag time series.
+    proc_param : dict
+        Processing parameters. e.g. date_begin, date_end (julian date)
+    ctime : list
+        Ctime values that should be calculated.
+    gain_in : dict
+         Input gain parameters of last calculation.
+         fields gpi, last_jd, nom, denom
+
+    Returns
+    -------
+    swi_ts : dict
+        Soil Water Index time series. Each field is for a ctime value.
+        fields jd, SWI_xxx, QFLAG_xxx (e.g. SWI_010 for ctime value 10)
+    gain : dict
+        Output gain parameters of last calculation. Each field is for a ctime value.
+        fields gpi, last_jd, NOM_xxx, DENOM_xxx (e.g. NOM_010 for ctime value 10)
+    """
+
+    ssm_ts = {'jd': jd,
+              'sm': ssm}
+
+    if 'date_from' in proc_param and 'date_to' in proc_param:
+        date_from = proc_param['date_from']
+        date_to = proc_param['date_to']
+    else:
+        date_from = np.min(ssm_ts['jd'])
+        date_to = np.max(ssm_ts['jd'])
+
+    num_swi = (date_to - date_from)
+    num_swi = num_swi.astype('timedelta64[D]')
+    num_swi = num_swi.astype(int)+1
+
+    if type(ctime) is not np.array:
+        ctime = np.array(ctime)
+
+    # Init empty swi_ts
+    swi_ts = {'jd': np.zeros([num_swi], dtype=np.float64),
+              'swi': np.zeros([num_swi, len(ctime)], dtype=np.float32),
+              'qflag': np.zeros([num_swi, len(ctime)], dtype=np.float32)}
+
+    swi_dates = np.arange(num_swi) + date_from
+
+    if num_swi != len(jd):
+        indices = [i for i, x in enumerate(ssm_ts['jd']) if x == date_from]
+        startdate_idx = indices[0]
+        ssm_ts['sm'] = ssm[startdate_idx:]
+        ssm_ts['jd'] = jd[startdate_idx:]
+
+    swi_ts['jd'] = swi_dates
+
+    gain = None
+
+    if gain_in is not None:
+        gain = inputdict_to_gain_dict(gain_in, ctime)
+
+    swi_ts, gain_out_mp = calc(ssm_ts, swi_ts, ssf_ts=ssf_ts, ctime=ctime, gain=gain)
+
+    swit_ts = swi_dict_to_outputdict(swi_ts, ctime)
+
+    gain_out = gain_dict_to_outputdict(gain_out_mp, ctime)
+
+    return swit_ts, gain_out
+
+
+def calc(ssm_ts, swi_ts, ssf_ts=None, gain=None, ctime=None):
     """
     Calculation of surface soil water index.
 
@@ -116,12 +196,11 @@ def calc(ssm_ts, ssf_ts, swi_ts, gain=None, ctime=None):
     ----------
     ssm_ts : dict
         Surface soil moisture time series.
-        fields sm, jd
-    ssf_ts : dict
-        Surface state flag time series.
-        fields ssf
+        fields sm, jd as numpy arrays.
     swi_ts : dict
         Empty SWI time series dictionary.
+    ssf_ts : numpy.ndarray
+        Surface state flag time series.
     gain : dict
         Gain parameters of last calculation.
         fields gpi, last_jd, nom, denom
@@ -150,36 +229,25 @@ def calc(ssm_ts, ssf_ts, swi_ts, gain=None, ctime=None):
         gain['nom'].fill(1)
 
 #   Only considering "non frozen" values (ssf = 1)
-    f_nan = float32_nan
+    juldate = ssm_ts['jd']
+    ssm = ssm_ts['sm']
 
-    frozen = np.where(ssf_ts['ssf'] != 1)[0]
+    if ssf_ts is not None:
+        f_nan = float32_nan
+        frozen = np.where(ssf_ts != 1)[0]
+        ssm_ts['sm'][frozen] = f_nan
+        valid = np.where(ssm_ts['sm'] != f_nan)[0]
+        if len(valid) == 0:
+            return swi_ts, gain
+        juldate = ssm_ts['jd'][valid]
+        ssm = ssm_ts['sm'][valid]
 
-    ssm_ts['sm'][frozen] = f_nan
-
-    valid = np.where(ssm_ts['sm'] != f_nan)[0]
-
-    if len(valid) == 0:
-        return swi_ts, gain
-
-    juldate = ssm_ts['jd'][valid]
-    ssm = ssm_ts['sm'][valid]
     ssm = np.asarray(ssm, dtype=np.float32)
-
-    date_buffer = pd.DatetimeIndex(juldate)
-
-    juldate_tmp = np.zeros([len(juldate)], dtype=np.float64)
-    for i in range(0, len(date_buffer)):
-        buf = julian.julday(date_buffer.month[i], date_buffer.day[i], date_buffer.year[i], date_buffer.hour[i],
-                            date_buffer.minute[i], date_buffer.second[i])
-        juldate_tmp[i] = buf
-
-    juldate = juldate_tmp
 
     norm_factor = \
         np.sum(np.exp(-(np.tensordot(np.arange(1000, dtype=np.float32),
                                      (1 / ctime), axes=0))), axis=0)
 
-#   last_jd_var is the index value of the gain file
     last_jd_var = gain['last_jd']
 
     if gain['denom'][0] == 1:
@@ -190,12 +258,8 @@ def calc(ssm_ts, ssf_ts, swi_ts, gain=None, ctime=None):
 
     swi_ts['swi'] = np.zeros([len(swi_ts['jd']), len(ctime)], dtype=np.float32)
     swi_ts['qflag'] = np.zeros([len(swi_ts['jd']), len(ctime)], dtype=np.float32)
-#   round juldate to make it possible to check equality.
-    jd_round = swi_ts['jd'].round(2)
-    juldate_round = juldate.round(2)
-    last_jd_var = last_jd_var.round(2)
 
-    swi_ts['swi'], swi_qflag, nom, denom, last_jd_var = swi_calc_cy(juldate_round, ssm, ctime, jd_round, swi_ts['swi'],
+    swi_ts['swi'], swi_qflag, nom, denom, last_jd_var = swi_calc_cy(juldate, ssm, ctime, swi_ts['jd'], swi_ts['swi'],
                                                                     swi_ts['qflag'], nom, denom, last_jd_var,
                                                                     norm_factor)
 
@@ -204,6 +268,31 @@ def calc(ssm_ts, ssf_ts, swi_ts, gain=None, ctime=None):
     return swi_ts, gain_out
 
 # --- Help functions ---
+
+
+def pd_datetime_to_juldate(pd_datetimes):
+    """
+    Converts panda.DatetimeIndex list format to a julian dates list.
+
+    Parameters
+    ----------
+    pd_datetimes : numpy.ndarray
+        Pandas Datetime array
+
+    Returns
+    -------
+    juldates : numpy.ndarray
+        Julian dates.
+    """
+    date_buffer = pd.DatetimeIndex(pd_datetimes)
+
+    juldate_tmp = np.zeros([len(pd_datetimes)], dtype=np.float64)
+    for i in range(0, len(date_buffer)):
+        buf = julian.julday(date_buffer.month[i], date_buffer.day[i], date_buffer.year[i], date_buffer.hour[i],
+                            date_buffer.minute[i], date_buffer.second[i])
+        juldate_tmp[i] = buf
+
+    return juldate_tmp
 
 
 def swi_dict_to_dataframe(swi_dict, ctime):
@@ -242,6 +331,102 @@ def swi_dict_to_dataframe(swi_dict, ctime):
     return swi_ts_df
 
 
+def swi_dict_to_outputdict(swi_dict, ctime):
+    """
+    Converts a swi dictionary format to a usable output dictionary
+    with names like SWI_010 as keys.
+
+    Parameters
+    ----------
+    swi_dict : dict
+        Soil Water Index time series as a dictionary.
+        fields gpi, jd, swi, qflag
+    ctime : numpy.ndarray
+        Integer values for the ctime variations.
+
+    Returns
+    -------
+    swi_ts_dict : dict
+        Soil Water Index time series. Each field is for a ctime value.
+        fields jd, SWI_xxx, QFLAG_xxx (e.g. SWI_010 for ctime value 10)
+    """
+    swi = swi_dict['swi']
+    jd = swi_dict['jd']
+    qflag = swi_dict['qflag']
+
+    dataframe_dict = {'jd': jd}
+
+    for i in range(0, len(ctime)):
+        dataframe_dict["SWI_%03d" % (ctime[i],)] = np.array([item[i] for item in swi])
+        dataframe_dict["QFLAG_%03d" % (ctime[i],)] = np.array([item[i] for item in qflag])
+
+    return dataframe_dict
+
+
+def gain_dict_to_outputdict(gain_dict, ctime):
+    """
+    Converts a gain(_out) dictionary format to an usable output format
+    with keyvalues for each ctime.
+
+    Parameters
+    ----------
+    gain_dict : dict
+        Gain parameters of last calculation.
+        fields gpi, last_jd, nom, denom
+    ctime : numpy.ndarray
+        Integer values for the ctime variations.
+
+    Returns
+    -------
+    gain_dict : dict
+        Gain parameters of last calculation. Each field is for a ctime value.
+        fields last_jd, NOM_xxx, DENOM_xxx (e.g. NOM_010 for ctime value 10)
+    """
+    last_jd = gain_dict['last_jd']
+
+    nom = gain_dict['nom']
+    denom = gain_dict['denom']
+
+    dataframe_dict = {}
+    dataframe_dict['last_jd'] = last_jd
+
+    for i in range(0, len(ctime)):
+        dataframe_dict["NOM_%03d" % (ctime[i],)] = nom[i]
+        dataframe_dict["DENOM_%03d" % (ctime[i],)] = denom[i]
+
+    return dataframe_dict
+
+def inputdict_to_gain_dict(gain_in, ctime):
+    """
+    Converts an input gain dictionary format to an algorithmusable gain dict format.
+    There the ctime index equals the gain index fot that ctime.
+
+    Parameters
+    ----------
+    gain_dict : dict
+        Gain parameters of last calculation.
+        fields gpi, last_jd, nom, denom
+    ctime : numpy.ndarray
+        Integer values for the ctime variations.
+
+    Returns
+    -------
+    gain_dict : dict
+        Gain parameters of last calculation.
+        fields last_jd, nom, denom
+    """
+    gain_dict = {}
+    gain_dict['last_jd'] = gain_in['last_jd']
+    gain_dict['nom'] = np.zeros(len(ctime))
+    gain_dict['denom'] = np.zeros(len(ctime))
+
+    for i in range(0, len(ctime)):
+        gain_dict['nom'][i] = gain_in["NOM_%03d" % (ctime[i],)]
+        gain_dict['denom'][i] = gain_in["DENOM_%03d" % (ctime[i],)]
+
+    return gain_dict
+
+
 def gain_dict_to_dataframe(gain_dict, ctime):
     """
     Converts a gain(_out) dictionary format to a pandas DataFrame format.
@@ -258,7 +443,7 @@ def gain_dict_to_dataframe(gain_dict, ctime):
     -------
     gain_dataframe : pandas.DataFrame
         Gain parameters of last calculation. Each field is for a ctime value.
-        fields gpi, last_jd(= date index), NOM_xxx, DENOM_xxx (e.g. NOM_010 for ctime value 10)
+        fields last_jd(= date index), NOM_xxx, DENOM_xxx (e.g. NOM_010 for ctime value 10)
     """
     last_jd = pd.to_datetime([gain_dict['last_jd'], gain_dict['last_jd']] - pd.Timestamp(0).to_julian_date(), unit='D')
 
@@ -292,7 +477,7 @@ def gain_dataframe_to_dict(gain_dataframe, ctime):
     -------
     gain_dict : dict
         Gain parameters of last calculation.
-        fields gpi, last_jd, nom, denom
+        fields last_jd, nom, denom
     """
 
     gain_dict = {}
--- tests/test_swi.py
+++ tests/test_swi.py
@@ -13,11 +13,11 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 """
 This module tests the soil water index calculation.
 """
-from pyswi.swi.swi import process
+from pyswi.swi.swi import process_uf_pd
+from pyswi.swi.swi import process_swi
 from pytesmo.time_series.filters import exp_filter
 import unittest
 import numpy as np
@@ -30,15 +30,11 @@ class SwiTest(unittest.TestCase):
         Setup I/O objects, configuration file and grid.
         """
 
-    def test_swi_calc(self):
+    def test_process_uf_pd_calc(self):
         """
         Test correct calculation of SWI, compared to the pytesmo calculation.
         """
         ctime = 5
-
-        proc_param = {}
-
-        swi_param = {'ctime': str([ctime])}
 
         dates = pd.date_range('2007-01-01', periods=9).to_julian_date().values
         pd_dates = pd.date_range('2007-01-01', periods=9)
@@ -46,7 +42,7 @@ class SwiTest(unittest.TestCase):
         ssm_userformat_data_test = pd.DataFrame({'jd': pd_dates, 'sm': [10, 20, 30, 40, 50, 60, 70, 80, 90],
                                                  'ssf': [1, 1, 1, 1, 1, 1, 1, 1, 1]}, index=pd_dates)
 
-        swi_ts_test, gain_test = process(ssm_userformat_data_test, proc_param, swi_param)
+        swi_ts_test, gain_test = process_uf_pd(ssm_userformat_data_test, ctime=[ctime])
 
         sm_in = ssm_userformat_data_test['sm'].values.astype(float)
 
@@ -56,17 +52,13 @@ class SwiTest(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(swi_ts, pytesmo_swi, 4)
 
-    def test_gain(self):
+    def test_process_uf_pd_gain(self):
         """
         Test gain in/out of the SWI calculation.
         It calculates the first 4 days and returns the value and gain and then starts the calculation of day 5 to 9 and
         compares it with pytesmo over the whole period.
         """
         ctime = 5
-
-        proc_param = {}
-
-        swi_param = {'ctime': str([ctime])}
 
         dates = pd.date_range('2007-01-01', periods=9).to_julian_date().values
         pd_dates = pd.date_range('2007-01-01', periods=9)
@@ -89,9 +81,9 @@ class SwiTest(unittest.TestCase):
         ssm_ut_part2 = pd.DataFrame({'jd': pd_dates_part2, 'sm': sm_data_part2,
                                      'ssf': ssf_data_part2}, index=pd_dates_part2)
 
-        swi_ts_part1, gain_test = process(ssm_ut_part1, proc_param, swi_param)
+        swi_ts_part1, gain_test = process_uf_pd(ssm_ut_part1, ctime=[ctime])
 
-        swi_ts_part2, gain_test = process(ssm_ut_part2, proc_param, swi_param, gain_test)
+        swi_ts_part2, gain_test = process_uf_pd(ssm_ut_part2, ctime=[ctime], gain_in=gain_test)
 
         sm_in = ssm_ut_whole['sm'].values.astype(float)
 
@@ -102,15 +94,11 @@ class SwiTest(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(swi_ts_ctime1+swi_ts_ctime2, pytesmo_swi, 5)
 
-    def test_ssf_frozen(self):
+    def test_process_uf_pd_ssf(self):
         """
         Test correct masking of frozen data for the SWI calculation.
         """
         ctime = 5
-
-        proc_param = {}
-
-        swi_param = {'ctime': str([ctime])}
 
         dates = pd.date_range('2007-01-01', periods=6).to_julian_date().values
         pd_dates = pd.date_range('2007-01-03', periods=9)
@@ -118,7 +106,7 @@ class SwiTest(unittest.TestCase):
         ssm_userformat_data_test = pd.DataFrame({'jd': pd_dates, 'sm': [10, 20, 30, 40, 50, 60, 70, 80, 90],
                                                  'ssf': [0, 2, 3, 1, 1, 1, 1, 1, 1]}, index=pd_dates)
 
-        swi_ts_test, gain_test = process(ssm_userformat_data_test, proc_param, swi_param)
+        swi_ts_test, gain_test = process_uf_pd(ssm_userformat_data_test, ctime=[ctime])
 
         sm_in = np.array([40, 50, 60, 70, 80, 90])
         sm_in = sm_in.astype(float)
@@ -128,6 +116,138 @@ class SwiTest(unittest.TestCase):
         swi_ts_ctime5 = swi_ts_test['SWI_005']
 
         np.testing.assert_array_almost_equal(swi_ts_ctime5[3:], pytesmo_swi, 4)
+
+    def test_process_uf_pd_time(self):
+        """
+        Test correct calculation of SWI, compared to the pytesmo calculation.
+        """
+        ctime = 5
+
+        pd_dates = pd.date_range('2007-01-01', periods=9)
+
+        dates_area = pd.date_range('2007-01-03', periods=3).to_julian_date().values
+
+        date_from = min(dates_area)
+        date_to = max(dates_area)
+
+        proc_param = {'date_from': date_from, 'date_to': date_to}
+
+        ssm_userformat_data_test = pd.DataFrame({'jd': pd_dates, 'sm': [10, 20, 30, 40, 50, 60, 70, 80, 90],
+                                                 'ssf': [1, 1, 1, 1, 1, 1, 1, 1, 1]}, index=pd_dates)
+
+        sm_pytesmo = np.array([30, 40, 50])
+
+        swi_ts_test, gain_test = process_uf_pd(ssm_userformat_data_test, ctime=[ctime], proc_param=proc_param)
+
+        sm_in = sm_pytesmo.astype(float)
+
+        pytesmo_swi = exp_filter(sm_in, np.asarray(dates_area, dtype=float), ctime=ctime)
+
+        swi_ts = swi_ts_test['SWI_005']
+
+        np.testing.assert_array_almost_equal(swi_ts, pytesmo_swi, 4)
+
+    def test_process_swi_calc(self):
+        """
+        Test correct calculation of SWI, compared to the pytesmo calculation.
+        """
+        ctime = 5
+
+        dates = pd.date_range('2007-01-01', periods=9).to_julian_date().values
+
+        sm = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+
+        swi_ts_test, gain_test = process_swi(sm, dates, ctime=[ctime])
+
+        sm_in = sm.astype(float)
+
+        pytesmo_swi = exp_filter(sm_in, np.asarray(dates, dtype=float), ctime=ctime)
+
+        swi_ts = swi_ts_test['SWI_005']
+
+        np.testing.assert_array_almost_equal(swi_ts, pytesmo_swi, 4)
+
+    def test_process_swi_gain(self):
+        """
+        Test gain in/out of the SWI calculation.
+        It calculates the first 4 days and returns the value and gain and then starts the calculation of day 5 to 9 and
+        compares it with pytesmo over the whole period.
+        """
+        ctime = 5
+
+        dates = pd.date_range('2007-01-01', periods=9).to_julian_date().values
+
+        sm_data = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+        dates_part1 = dates[:-4]
+        dates_part2 = dates[5:]
+        sm_data_part1 = np.array(sm_data[:-4])
+        sm_data_part2 = np.array(sm_data[5:])
+
+        swi_ts_part1, gain_test = process_swi(sm_data_part1, dates_part1, ctime=[ctime])
+
+        swi_ts_part2, gain_test = process_swi(sm_data_part2, dates_part2, ctime=[ctime], gain_in=gain_test)
+
+        sm_in = np.array(sm_data).astype(float)
+
+        pytesmo_swi = exp_filter(sm_in, np.asarray(dates, dtype=float), ctime=ctime)
+
+        swi_ts_ctime1 = swi_ts_part1['SWI_005'].tolist()
+        swi_ts_ctime2 = swi_ts_part2['SWI_005'].tolist()
+
+        np.testing.assert_array_almost_equal(swi_ts_ctime1+swi_ts_ctime2, pytesmo_swi, 5)
+
+    def test_process_swi_ssf(self):
+        """
+        Test correct masking of frozen data for the SWI calculation.
+        """
+        ctime = 5
+
+        dates = pd.date_range('2007-01-01', periods=6).to_julian_date().values
+        dates_whole = pd.date_range('2007-01-03', periods=9).to_julian_date().values
+
+        sm = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+        ssf = np.array([0, 2, 3, 1, 1, 1, 1, 1, 1])
+
+        swi_ts_test, gain_test = process_swi(sm, dates_whole, ssf_ts=ssf, ctime=[ctime])
+
+        sm_in = np.array([40, 50, 60, 70, 80, 90])
+        sm_in = sm_in.astype(float)
+
+        pytesmo_swi = exp_filter(sm_in, np.asarray(dates, dtype=float), ctime=ctime)
+
+        swi_ts_ctime5 = swi_ts_test['SWI_005']
+
+        np.testing.assert_array_almost_equal(swi_ts_ctime5[3:], pytesmo_swi, 4)
+
+    def test_process_swi_time(self):
+        """
+        Test correct calculation of SWI, compared to the pytesmo calculation.
+        """
+        ctime = 5
+
+        dates = pd.date_range('2007-01-01', periods=9).to_julian_date().values
+
+        dates_area = pd.date_range('2007-01-03', periods=3).to_julian_date().values
+
+        date_from = min(dates_area)
+        date_to = max(dates_area)
+
+        proc_param = {'date_from': date_from, 'date_to': date_to}
+
+        sm = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+
+        sm_pytesmo = np.array([30, 40, 50])
+
+        swi_ts_test, gain_test = process_swi(sm, dates, ctime=[ctime], proc_param=proc_param)
+
+        sm_in = sm_pytesmo.astype(float)
+
+        pytesmo_swi = exp_filter(sm_in, np.asarray(dates_area, dtype=float), ctime=ctime)
+
+        swi_ts = swi_ts_test['SWI_005']
+
+        np.testing.assert_array_almost_equal(swi_ts, pytesmo_swi, 4)
 
 
 if __name__ == '__main__':
