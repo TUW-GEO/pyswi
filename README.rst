9f118488afdc0d8233cd7da10a454ed3d1ba6113
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2017-03-30 09:56:28 +0200

    cython setup.py improvement

--- README.rst
+++ README.rst
@@ -19,16 +19,9 @@ In the root directory of the repository run the following command to create a ne
 
 conda env create -f conda_environment.yml
 
-
 The new environment can be activated by calling:
 
 source activate swi_env
-
-
-swi_calc_routines.pyx has to be compiled using the following commands (Linux):
-
-cython -a swi_calc_routines.pyx
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o swi_calc_routines.so swi_calc_routines.c
 
 Note
 ====
--- setup.py
+++ setup.py
@@ -10,13 +10,53 @@
 
 import sys
 from setuptools import setup
+from distutils.cmd import Command
+from distutils.command.build_ext import build_ext as _build_ext
+from distutils.extension import Extension
+import pkg_resources
+
+class Cythonize(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are
+        # up-to-date
+        from Cython.Build import cythonize
+        cythonize(['pytswi/swi/swi_calc_routiones.pyx'])
+
+
+class NumpyBuildExt(_build_ext):
+
+    def build_extensions(self):
+        numpy_incl = pkg_resources.resource_filename('numpy', 'core/include')
+
+        for ext in self.extensions:
+            if hasattr(ext, 'include_dirs') and not numpy_incl in ext.include_dirs:
+                ext.include_dirs.append(numpy_incl)
+        _build_ext.build_extensions(self)
+
+ext_modules = [Extension("pyswi.swi.swi_calc_routines",
+                         ["pyswi/swi/swi_calc_routines.c"], include_dirs=[]), ]
+
 
 
 def setup_package():
+    cmdclass = {}
+    cmdclass['cythonize'] = Cythonize
+    cmdclass['build_ext'] = NumpyBuildExt
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
           tests_require=['test_swi'],
+          cmdclass=cmdclass,
+          ext_modules=ext_modules,
           use_pyscaffold=True)
 
 
