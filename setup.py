d6cd796297096a1c71a7195b60d54e27013765ae
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2018-05-18 15:05:49 +0200

    add precompiled cython library

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ class NumpyBuildExt(_build_ext):
         _build_ext.build_extensions(self)
 
 ext_modules = [Extension("pyswi.swi_ts.swi_calc_routines",
-                         ["pyswi/swi_ts/swi_calc_routines_.c"], include_dirs=[]), ]
+                         ["pyswi/swi_ts/swi_calc_routines.c"], include_dirs=[]), ]
 
 
 
